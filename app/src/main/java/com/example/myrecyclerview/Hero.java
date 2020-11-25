aced234c279339aed3421e0c9485a7a087c20a34
arifin <arifin.nurhidayah1933@gmail.com>
2020-11-25 20:25:32 +0700

    PahlawanApp

---
+++ app/src/main/java/com/example/myrecyclerview/Hero.java
@@ -0,0 +1,69 @@
+package com.example.myrecyclerview;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+public class Hero implements Parcelable {
+    private String name;
+
+    protected Hero(Parcel in) {
+        name = in.readString();
+        description = in.readString();
+        photo = in.readString();
+    }
+
+    public static final Parcelable.Creator<Hero> CREATOR = new Parcelable.Creator<Hero>() {
+        @Override
+        public Hero createFromParcel(Parcel source) {
+            return new Hero(source);
+        }
+
+        @Override
+        public Hero[] newArray(int size) {
+            return new Hero[size];
+        }
+    };
+
+    public Hero() {
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getPhoto() {
+        return photo;
+    }
+
+    public void setPhoto(String photo) {
+        this.photo = photo;
+    }
+
+    private String description;
+    private String photo;
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(this.name);
+        dest.writeString(this.description);
+        dest.writeString(this.photo);
+    }
+}
